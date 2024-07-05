f2af70d29bda698a513beedca0d1fdd529145e7d
Damitha <damitha.arachchige@sensium.co.uk>
2024-07-05 12:14:40 +0100

    After loginviewmodel the firebase token is sent

--- app/src/main/java/com/okada/android/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/okada/android/ui/login/LoginViewModel.kt
@@ -14,9 +14,12 @@ import com.okada.android.R
 import com.okada.android.data.AccountUsecase
 import com.okada.android.data.ProfileUsecase
 import com.okada.android.data.model.DriverInfo
+import com.okada.android.data.model.LoggedInUser
+import com.okada.android.data.model.TokenModel
 import com.okada.android.ui.splash.SplashResult
 
-class LoginViewModel(private val loginRepository: AccountUsecase, private val profileUsecase: ProfileUsecase) : ViewModel() {
+class LoginViewModel(private val loginRepository: AccountUsecase,
+                     private val profileUsecase: ProfileUsecase) : ViewModel() {
 
     private val _loginForm = MutableLiveData<LoginFormState>()
     val loginFormState: LiveData<LoginFormState> = _loginForm
@@ -27,29 +30,53 @@ class LoginViewModel(private val loginRepository: AccountUsecase, private val pr
     fun login(username: String, password: String) {
         loginRepository.login(username, password) {result ->
             result.fold(onSuccess = {user ->
-//check if the logged in user has a profile
-                profileUsecase.checkProfileExists(user) {result ->
-                    result.fold(onSuccess = { profile ->
+            //check if the logged in user has a profile
+                sendFirebaseToken(user)
+            },onFailure = {
+                _loginResult.value = LoginResult(errorMsg = it.message)
+            })
+        }
+    }
+
+    private fun sendFirebaseToken(user: LoggedInUser) {
+        loginRepository.fetchPushNotificationToken {result->
+            result.fold(onSuccess = { token ->
+                val model = TokenModel()
+                model.token = token
+                profileUsecase.sendPushNotificationToken(user.userId, model) {result->
+                    result.fold(onSuccess = {
                         //check if the logged in user has a profile
-                        Log.i("okada Log","LoginViewModel profile rxed ! ${profile!=null}")
-                        profile?.also {user->
-                            //-> Goto home screen
-                            Log.i("okada Log","LoginViewModel Goto home screen!")
-                            Common.currentUser = user
-                            _loginResult.value =
-                                LoginResult(navigateToHome = true)
-                        } ?: run {
-                            // No-> goto register screen
-                            Log.i("okada Log","LoginViewModel Goto register screen!")
-                            _loginResult.value =
-                                LoginResult(navigateToRegister = true)
-                        }
+                        Log.i("App_info","sendFirebaseToken, Token sent: $token")
+                        checkProfile(user)
                     }, onFailure = {
-                        // Error occurred
-                        _loginResult.value = LoginResult(errorMsg = it.message)
+                        Log.i("App_info","sendFirebaseToken, Error: ${it.message}")
                     })
                 }
-            },onFailure = {
+            }, onFailure = {
+                _loginResult.value = LoginResult(errorMsg = it.message)
+            })
+        }
+    }
+
+    private fun checkProfile(user: LoggedInUser) {
+        profileUsecase.checkProfileExists(user) {result ->
+            result.fold(onSuccess = { profile ->
+                //check if the logged in user has a profile
+                Log.i("okada Log","LoginViewModel profile rxed ! ${profile!=null}")
+                profile?.also {user->
+                    //-> Goto home screen
+                    Log.i("okada Log","LoginViewModel Goto home screen!")
+                    Common.currentUser = user
+                    _loginResult.value =
+                        LoginResult(navigateToHome = true)
+                } ?: run {
+                    // No-> goto register screen
+                    Log.i("okada Log","LoginViewModel Goto register screen!")
+                    _loginResult.value =
+                        LoginResult(navigateToRegister = true)
+                }
+            }, onFailure = {
+                // Error occurred
                 _loginResult.value = LoginResult(errorMsg = it.message)
             })
         }
