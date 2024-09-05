825bae3201763f3b0114905628956d3dd19c3b90
Damitha <damitha.arachchige@sensium.co.uk>
2024-09-05 14:13:14 +0100

    2.6 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okada.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "2.5"
+        versionCode = 8
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
